1687b9cfdae8932702e9d3af799cae62eee0c992
DZMITRY KUZMITCH <kuzya19989@outlook.com>
2020-02-19 22:43:29 -0500

    Changed to regular PHP script

--- index.php
+++ index.php
@@ -1,15 +1,5 @@
-<html>
-
-<head>
-    <title>Awesome Site!</title>
-</head>
-
-<body>
-    <?php
-        $_POST = json_decode(file_get_contents('php://input'), true);
-        header('Content-Type: application/json');
-        echo json_encode($_POST);
-    ?>
-</body>
-
-</html>
+<?php
+    $_POST = json_decode(file_get_contents('php://input'), true);
+    header('Content-Type: application/json');
+    echo json_encode($_POST);
+?>
