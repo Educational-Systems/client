b45b4dd221b2567885700f38977ae7c31d89ae83
DZMITRY KUZMITCH <kuzya19989@outlook.com>
2020-02-19 22:33:51 -0500

    Added simple POST request

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+var login_form = document.getElementById("login-form");
+var submit_btn = document.getElementById("submit-btn");
+
+login_form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    var data = {};
+    var formData = new FormData(e.target);
+
+    for (var [key, value] of formData.entries()) {
+        data[key] = value;
+    }
+
+    const http = new XMLHttpRequest();
+    const url = 'https://scrawlless.com/api/test';
+
+    http.open("POST", url, true);
+    http.setRequestHeader("Content-type", "application/json");
+    http.send(JSON.stringify(data));
+
+    http.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(JSON.parse(http.responseText));
+        }
+    }
+
+    return false;
+});
