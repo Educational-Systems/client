b494b702d9897f751508b54d3871d84edefda3e4
DZMITRY KUZMITCH <kuzya19989@outlook.com>
2020-02-23 20:02:21 -0500

    TEsting

--- login.php
+++ login.php
@@ -1,17 +1,22 @@
 <?php
-    $_POST = json_decode(file_get_contents('php://input'), true); // Receiving JSON data from the JavaScript Client
-    $data = json_encode($_POST); // Encoding that data to pass it
+    //$_POST = json_decode(file_get_contents('php://input'), true); // Receiving JSON data from the JavaScript Client
+    //$data = json_encode($_POST); // Encoding that data to pass it
+    //$ch = curl_init('https://web.njit.edu/~npm26/logingate.php'); // Test URL
 
-    $ch = curl_init('https://web.njit.edu/~npm26/logingate.php'); // Test URL
+
+
+    $ch = curl_init('https://myhub.njit.edu/vrs/ldapAuthenticateServlet'); // Test URL
+
     curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST"); // HTTP request method
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $data); // Passing data to the request
+    curl_setopt($ch, CURLOPT_POSTFIELDS, "usern_name=dk497&passwd=%24115Mamulya&SUBMIT=Login");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // Whatever
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json', 'Content-Length: ' . strlen($data))); // Setting necessary headers
 
     $result = curl_exec($ch); // Executing cURL HTTP request
 
     curl_close($ch); // Closing request
 
-    header('Content-Type: application/json'); // Setting header for our PHP response
-    echo $result; // Returning result
+    echo $result;
+
+    //header('Content-Type: application/json'); // Setting header for our PHP response
+    //echo $result; // Returning result
 ?>
