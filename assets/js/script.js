7277c22570ff55201f3d05543b07abd2b7265594
DZMITRY KUZMITCH <kuzya19989@outlook.com>
2020-05-02 14:29:41 -0400

    added url encoding

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,10 +29,19 @@ function toggle_response(state, data = null) {
 
 function middleTest() {
     var data = {};
+    var solution = document.getElementById('test').value;
+    var encoded_solution = encodeURI(solution);
+    var decoded_solution = decodeURI(encoded_solution);
 
-    data = { "questionID": "32", "points": "77", "solution": "def add(a, b): return(a + b)", "function_name": "add", "function_name_points": 1, "constraint": "print", "constraint_points": 2, "colon_points": 3, "input1": "1, 2", "input2": "3, 4", "input3": "7, 8", "input4": "-1, 1", "input5": "2, 2", "input6": "0, 0", "output1": "3", "output2": "7", "output3": "15", "output4": "0", "output5": "4", "output6": "0", "output1_points": 4, "output2_points": 5, "output3_points": 6, "output4_points": 7, "output5_points": 8, "output6_points": 9 };
+    data = { "questionID": "32", "points": "77", "function_name": "add", "function_name_points": 1, "constraint": "print", "constraint_points": 2, "colon_points": 3, "input1": "1, 2", "input2": "3, 4", "input3": "7, 8", "input4": "-1, 1", "input5": "2, 2", "input6": "0, 0", "output1": "3", "output2": "7", "output3": "15", "output4": "0", "output5": "4", "output6": "0", "output1_points": 4, "output2_points": 5, "output3_points": 6, "output4_points": 7, "output5_points": 8, "output6_points": 9 };
+
     const http = new XMLHttpRequest();
-    const url = pre_url + 'api/middle_test.php';
+    const url = pre_url + 'api/middle_test.php?solution=' + encoded_solution;
+
+    console.log(solution);
+    console.log(encoded_solution);
+    console.log(decoded_solution);
+    console.log(url);
 
     http.open("POST", url, true);
     http.setRequestHeader("Content-type", "application/json");
