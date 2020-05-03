2cc1b4258611c1e6e080cf95d9309571c5634959
DZMITRY KUZMITCH <kuzya19989@outlook.com>
2020-05-03 16:42:51 -0400

    Added new encoding function

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,11 +27,32 @@ function toggle_response(state, data = null) {
 
 }
 
+function urlencode(str) {
+    str = (str + '').toString();
+    return encodeURIComponent(str)
+        .replace('!', '%21')
+        .replace('\'', '%27')
+        .replace('(', '%28')
+        .replace(')', '%29')
+        .replace('*', '%2A')
+        .replace('+', '%20');
+}
+
+function urldecode(str) {
+    str = (str + '').toString();
+    return decodeURIComponent(str)
+        .replace('%21', '!')
+        .replace('%27', '\'')
+        .replace('%28', '(')
+        .replace('%29', ')')
+        .replace('%2A', '*')
+        .replace('%20', '+');
+}
 function middleTest() {
     var data = {};
     var solution = document.getElementById('test').value;
-    var encoded_solution = encodeURI(solution);
-    var decoded_solution = decodeURI(encoded_solution);
+    var encoded_solution = urlencode(solution);
+    var decoded_solution = urldecode(encoded_solution);
 
     data = { "questionID": "32", "points": "77", "function_name": "add", "function_name_points": 1, "constraint": "print", "constraint_points": 2, "colon_points": 3, "input1": "1, 2", "input2": "3, 4", "input3": "7, 8", "input4": "-1, 1", "input5": "2, 2", "input6": "0, 0", "output1": "3", "output2": "7", "output3": "15", "output4": "0", "output5": "4", "output6": "0", "output1_points": 4, "output2_points": 5, "output3_points": 6, "output4_points": 7, "output5_points": 8, "output6_points": 9 };
 
